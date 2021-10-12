4eaae75062d3847bcb18fac1547b696d44939fde
opwe37 <opwe37@naver.com>
2021-10-13 04:43:39 +0900

    리팩토링

--- imgdescribeapp/ml/predict.py
+++ imgdescribeapp/ml/predict.py
@@ -2,67 +2,53 @@ import torch
 
 from transformers import BertTokenizer
 from PIL import Image
-import argparse
 
-from .models import caption
-from .datasets import coco, utils
+from .datasets import coco
 from .configuration import Config
-import os
 
-# parser = argparse.ArgumentParser(description='Image Captioning')
-# parser.add_argument('--path', type=str, help='path to image', required=True)
-# args = parser.parse_args()
-# image_path = args.path
-
-model = None;
+config = Config()
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+model = None
 if not model:
     model = torch.hub.load('saahiluppal/catr', 'v3', pretrained=True)
 
+
+def create_caption_and_mask(start_token, max_length):
+    caption_template = torch.zeros((1, max_length), dtype=torch.long)
+    mask_template = torch.ones((1, max_length), dtype=torch.bool)
+
+    caption_template[:, 0] = start_token
+    mask_template[:, 0] = False
+
+    return caption_template, mask_template
+
+@torch.no_grad()
+def evaluate(image, caption, cap_mask):
+    model.eval()
+    for i in range(config.max_position_embeddings - 1):
+        predictions = model(image, caption, cap_mask)
+        predictions = predictions[:, i, :]
+        predicted_id = torch.argmax(predictions, axis=-1)
+
+        if predicted_id[0] == 102:
+            return caption
+
+        caption[:, i+1] = predicted_id[0]
+        cap_mask[:, i+1] = False
+
+    return caption
+
+
 def get_caption(image_path):
-    config = Config()
-
-    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-
     start_token = tokenizer.convert_tokens_to_ids(tokenizer._cls_token)
-    end_token = tokenizer.convert_tokens_to_ids(tokenizer._sep_token)
 
     image = Image.open(image_path)
     image = coco.val_transform(image)
     image = image.unsqueeze(0)
 
+    caption, cap_mask = create_caption_and_mask(start_token, config.max_position_embeddings)
 
-    def create_caption_and_mask(start_token, max_length):
-        caption_template = torch.zeros((1, max_length), dtype=torch.long)
-        mask_template = torch.ones((1, max_length), dtype=torch.bool)
-
-        caption_template[:, 0] = start_token
-        mask_template[:, 0] = False
-
-        return caption_template, mask_template
-
-
-    caption, cap_mask = create_caption_and_mask(
-        start_token, config.max_position_embeddings)
-
-
-    @torch.no_grad()
-    def evaluate():
-        model.eval()
-        for i in range(config.max_position_embeddings - 1):
-            predictions = model(image, caption, cap_mask)
-            predictions = predictions[:, i, :]
-            predicted_id = torch.argmax(predictions, axis=-1)
-
-            if predicted_id[0] == 102:
-                return caption
-
-            caption[:, i+1] = predicted_id[0]
-            cap_mask[:, i+1] = False
-
-        return caption
-
-
-    output = evaluate()
+    output = evaluate(image, caption, cap_mask)
     result = tokenizer.decode(output[0].tolist(), skip_special_tokens=True)
 
     print(result.capitalize())
